2464e2e128a9c545c703b343a32def05384ae80f
cassiodasilva231-cmyk <cassiodasilva231@gmail.com>
2026-02-28 22:03:53 -0300

    Update codes.js

--- codes.js
+++ codes.js
@@ -6,10 +6,23 @@ localStorage.setItem("codigosVIP", JSON.stringify(codigosVIP));
 
 function gerarCodigo(){
 let codigo = "VIP-" + Math.random().toString(36).substring(2,10).toUpperCase();
+
 codigosVIP[codigo] = {
 usado:false,
-data:Date.now()
+expira: Date.now() + (30 * 24 * 60 * 60 * 1000) // 30 dias
 };
+
 salvarCodigos();
 return codigo;
 }
+
+function verificarExpirados(){
+for(let c in codigosVIP){
+if(Date.now() > codigosVIP[c].expira){
+delete codigosVIP[c];
+}
+}
+salvarCodigos();
+}
+
+verificarExpirados();
