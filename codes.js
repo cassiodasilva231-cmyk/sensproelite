b04cb95857add88614e6ca61184f092f62d810a2
cassiodasilva231-cmyk <cassiodasilva231@gmail.com>
2026-02-28 21:55:40 -0300

    Modify gerarCodigo to return a longer VIP code
    Updated the length of the generated VIP code and modified the gerarCodigo function to return the generated code.

--- codes.js
+++ codes.js
@@ -5,27 +5,11 @@ localStorage.setItem("codigosVIP", JSON.stringify(codigosVIP));
 }
 
 function gerarCodigo(){
-let codigo = "VIP-" + Math.random().toString(36).substring(2,8).toUpperCase();
-
+let codigo = "VIP-" + Math.random().toString(36).substring(2,10).toUpperCase();
 codigosVIP[codigo] = {
-usado:false
+usado:false,
+data:Date.now()
 };
-
 salvarCodigos();
-atualizarLista();
+return codigo;
 }
-
-function atualizarLista(){
-let lista = document.getElementById("listaCodigos");
-if(!lista) return;
-
-lista.innerHTML = "";
-
-for(let codigo in codigosVIP){
-let li = document.createElement("li");
-li.innerText = codigo + (codigosVIP[codigo].usado ? " (usado)" : "");
-lista.appendChild(li);
-}
-}
-
-window.onload = atualizarLista;
