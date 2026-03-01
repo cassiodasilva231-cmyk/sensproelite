ae678a4653bdb1aba244a3322e7835aa750f6648
cassiodasilva231-cmyk <cassiodasilva231@gmail.com>
2026-02-28 21:42:11 -0300

    Update codes.js

--- codes.js
+++ codes.js
@@ -3,3 +3,29 @@ let codigosVIP = JSON.parse(localStorage.getItem("codigosVIP")) || {};
 function salvarCodigos(){
 localStorage.setItem("codigosVIP", JSON.stringify(codigosVIP));
 }
+
+function gerarCodigo(){
+let codigo = "VIP-" + Math.random().toString(36).substring(2,8).toUpperCase();
+
+codigosVIP[codigo] = {
+usado:false
+};
+
+salvarCodigos();
+atualizarLista();
+}
+
+function atualizarLista(){
+let lista = document.getElementById("listaCodigos");
+if(!lista) return;
+
+lista.innerHTML = "";
+
+for(let codigo in codigosVIP){
+let li = document.createElement("li");
+li.innerText = codigo + (codigosVIP[codigo].usado ? " (usado)" : "");
+lista.appendChild(li);
+}
+}
+
+window.onload = atualizarLista;
