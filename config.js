ad620604614bee5bef4da1996d7d8380a4ef0d9a
cassiodasilva231-cmyk <cassiodasilva231@gmail.com>
2026-03-02 14:04:48 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = "https://zrsyfkjmykgkloztgili.supabase.co"
+const SUPABASE_KEY = "sb_publishable_-p2uZXfW5GlKe6G60xry0A_nfsTKwK5"
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
